413705e84ffd12df0cbd65e84a1410b27231b47e
James Wurbel <james.wurbel@clover.com>
2024-09-01 18:10:05 -0700

    Add Terraform configuration for TFE team and Vault secret management

--- variables.tf
+++ variables.tf
@@ -33,3 +33,30 @@ variable "address" {
   description = "Vault Address"
   
 }
+
+variable create_team {
+  type = bool
+  description = "Create a new team"
+  default = false
+}
+
+variable team_visibility {
+  type = string
+  description = "Team Visibility"
+  default = "secret"
+}
+
+variable organization_access {
+  type = object({
+    manage_modules          = optional(bool, false)
+    manage_policies         = optional(bool, false)
+    manage_policy_overrides = optional(bool, false)
+    manage_providers        = optional(bool, false)
+    manage_vcs_settings     = optional(bool, false)
+    manage_workspaces       = optional(bool, false)
+    read_workspaces         = optional(bool, false)
+    read_projects           = optional(bool, false)
+  })
+  description = "Organization Access"
+  default = {}
+}
